f098f38555fa2f45f04fdb24fd45ce133ddd6875
Greg Walters <Greg@GregWWalters.dev>
2024-05-30 23:51:59 -0500

    start doc.go

---
+++ doc.go
@@ -0,0 +1,6 @@
+// Package config provides a means for defining application configuration
+// variables that can be set by multiple means. In order of least to greatest
+// precedence, variables may be set by environment variables, an environment
+// file, or command-line flags. The variables may be changed after the program
+// starts up.
+package config
